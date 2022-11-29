0da1fdf1c08d92d9f51a79986bcdbd028a10353c
Osama Abozahra <o.abozahra@ncgr.gov.sa>
2022-11-29 23:11:49 +0200

    adding file of the game

---
+++ story_content/user.js
@@ -0,0 +1,17 @@
+function ExecuteScript(strId) {
+    switch (strId) {
+        case "6ZpdF6EPBS2":
+            Script1();
+            break;
+    }
+}
+
+function Script1() {
+    var player = GetPlayer();
+    var x = player.GetVar("myname");
+//write your code here ya osos
+    console.log('Trying to access the parent document form the iframe');
+    console.log(window);
+    console.log(window.parent);
+}
+
